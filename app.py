7290296b05e9bc77cd69c1258377c015db569086
Debayan Roy <144272628+debayanroy676@users.noreply.github.com>
2025-10-21 12:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
